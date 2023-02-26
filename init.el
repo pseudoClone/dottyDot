6901bbc7821aec46a16322389acb47572c83a4c7
pseudoclone <prince.077bei27@acem.edu.np>
2023-02-26 10:20:31 +0545

    refactor: Add use-package, more comments, 80 line rule and faster load

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; Emacs default variables
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -5,9 +6,7 @@
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
  '(column-number-mode t)
- '(custom-enabled-themes '(wheatgrass doom-acario-dark))
- '(custom-safe-themes
-   '("2dd4951e967990396142ec54d376cced3f135810b2b69920e77103e0bcedfba9" default))
+ '(custom-enabled-themes '(wheatgrass))
  '(fringe-mode 0 nil (fringe))
  '(global-display-line-numbers-mode t)
  '(package-selected-packages
@@ -15,74 +14,98 @@
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
  '(tooltip-mode nil))
+
+;; Set faces/fonts
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "JetBrains Mono NL" :foundry "JB" :slant normal :weight semi-bold :height 113 :width normal)))))
+
+;; Yes or no to y or n
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+
+;; No startup screen
 (setq inhibit-startup-screen t)
+
+
+;; No autosave
 (setq auto-save-default nil)
 
+;; Faster startup supressing garbage collector
+(setq gc-cons-threshold 100000000)
+
+;; Makes *scratch* empty.
+(setq initial-scratch-message "")
+
+
+;; Removes *scratch* from buffer after the mode has been set.
+(defun remove-scratch-buffer ()
+  (if (get-buffer "*scratch*")
+      (kill-buffer "*scratch*")))
+(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
+
+;; Enable Melpa
+(use-package package
+  :config
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (package-initialize))
+
+
+;; Column and Whitespace
+;; https://web.stanford.edu/class/cs107/resources/emacs_advanced
+(use-package whitespace
+  :config
+  (setq whitespace-style '(face lines-tail))
+  (setq whitespace-line-column 80) ;; Or whatever other column number.
+  (global-whitespace-mode t))
+
+
+;; Yasnippet Enable
+(use-package yasnippet
+  :config
+  (yas-global-mode 1))
+
+
+;; Word Wrap Global
 (global-visual-line-mode t)
 
-(global-set-key (kbd "C-<tab>") 'next-buffer)
-(global-set-key (kbd "C-<iso-lefttab>") 'previous-buffer)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;Source:https://caiorss.github.io/Emacs-Elisp-Programming/Customization.html
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;https://caiorss.github.io/Emacs-Elisp-Programming/Customization.html
 (defun display-startup-echo-area-message ()
   ;; "If it wasn't for this you'd be GNU/Spammed by now"
   (message ""))
+
+
 
 ;; Don't insert instructions in the *scratch* buffer
 (setq initial-scratch-message nil)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
 
+
+;; No backup files
 (setq make-backup-files nil)
 
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;;Company-mode config
-;; (use-package company
-;;      :ensure t
-;;      :init
-;;      (add-hook 'after-init-hook 'global-company-mode))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PowerLine centre config
+(use-package powerline
+  :config
+  (powerline-center-theme))
 
 
 
-(require 'powerline)
-(powerline-center-theme)
-
-
-
+;; Enables parenthesis closing and region insertion
 (electric-pair-mode 1)
 (setq sgml-quick-keys 'close)
 
-;;Vertico
-(require 'vertico)
-(vertico-mode)
 
 
-;;Yasnippet
-(add-to-list 'load-path
-              "~/.emacs.d/plugins/yasnippet")
-(require 'yasnippet)
-(yas-global-mode 1)
-
-
-(global-set-key (kbd "C-c <left>")  'windmove-left)
-(global-set-key (kbd "C-c <right>") 'windmove-right)
-(global-set-key (kbd "C-c <up>")    'windmove-up)
-(global-set-key (kbd "C-c <down>")  'windmove-down)
+;;Vertico Mode
+(use-package vertico
+  :init
+  (vertico-mode))
